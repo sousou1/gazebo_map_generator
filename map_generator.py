25178f6e6bf837881aa2636d50fe97abf3213ded
sousou1 <tamusou6953@gmail.com>
2018-08-22 19:46:06 +0900

    map_generator

---
+++ map_generator.py
@@ -0,0 +1,45 @@
+import math
+
+result_str = ''
+
+user_name = 'sou'
+
+map_array = [[[2, 1], [3, 2], [2, 3]],
+			[[2, 1], [3, 2], [2, 3]]]
+
+# 二次元で指定
+# [[2,1], [3,2], [4,1], [5,1]] [[3,2], [4,1], [0,0], [2,3]]
+# 一つ目でmapデータの種類(road1~8) 2つ目で回転角
+
+y_count = 0
+for y in map_array:
+	y_count += 1
+
+y_middle = int(y_count / 2)
+
+y_count = 0
+road_count = 1
+for y in map_array:
+	x_count = 0
+	for x in y:
+		result_str += '''
+    <model name="road''' + str(road_count) + '''">
+      <pose>''' + str(x_count) + ' ' + str(y_middle - y_count) + ' ' + '0  0 0 ' + "{0:.5f}".format(1.57079 - x[1] * 1.57079) + '''</pose>
+      <static>true</static>
+      <link name="body">
+        <visual name="visual">
+          <geometry>
+            <mesh>
+               <uri>file:///home/''' + user_name + '/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/road_meshes/road'
+		result_str += str(x[0]) + '''.dae</uri>
+               <scale>0.5 0.5 0.5</scale>
+               </mesh>
+          </geometry>
+        </visual>
+      </link>
+    </model>'''
+		road_count += 1
+		x_count += 1
+	y_count += 1
+
+print(result_str)
